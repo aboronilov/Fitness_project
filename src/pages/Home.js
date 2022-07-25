90177fa8a81e6888c2c80da70cbad1163afbe597
Anatoly <a.boronilov@infoculture.ru>
2022-07-25 16:42:07 +0200

    hero banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import { Box } from '@mui/material';
 import React, {useState} from 'react';
 
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
+
 const Home = () => {
     return (
         <Box>
