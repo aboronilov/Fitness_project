dfe2ff814e533dab7edf5cd64d4706b19f2aa291
Anatoly <a.boronilov@infoculture.ru>
2022-07-25 15:44:08 +0200

    Navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React, {useState} from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Box>
+            <HeroBanner/>
+            <SearchExercises/>
+            <Exercises/>
+        </Box>
     );
 }
 
