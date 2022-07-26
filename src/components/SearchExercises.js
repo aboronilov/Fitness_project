dd1ac959e1c0b6a4ec736bedd1926793cc6943d4
Anatoly <a.boronilov@infoculture.ru>
2022-07-26 09:43:56 +0200

    search backend

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,34 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { fetchData, exerciseOptions } from '../utils/fetchData';
 
 
 const SearchExercises = () => {
     const [search, setSearch] = useState("");
+    const [exercises, setExercises] = useState([]);
+    const [bodyParts, setBodyParts] = useState([]);
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+            setBodyParts(['all', ...bodyPartsData])
+        }
+        fetchExercisesData()
+    }, []);
+
+    const handleSearch = async () => {
+        if (search) {
+            const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+            const searchResult = exercisesData.filter(
+                (exercise)=>exercise.name.toLowerCase().includes(search)
+                || exercise.target.toLowerCase().includes(search)
+                || exercise.bodyPart.toLowerCase().includes(search)
+                || exercise.equipement.toLowerCase().includes(search)
+            )
+            setSearch('');
+            setExercises(searchResult);
+        };
+    }
     return (
         <Stack 
             alignItems="center" 
@@ -51,6 +76,7 @@ const SearchExercises = () => {
                         // p: "16px",
                         // ml: "15px"
                     }}
+                    onClick={handleSearch}
                 >
                     Search
                 </Button>
